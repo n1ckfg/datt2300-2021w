e073afe98aa74d42b8e74ac14933826904452500
Nick Fox-Gieg <nick@fox-gieg.com>
2021-03-26 09:24:53 -0400

    move script

---
+++ week10/example10-001/Assets/Scripts/MoverExample.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverExample : MonoBehaviour {
+
+    public float speed = 5f;
+    public float range = 2f;
+
+    private void Start() {
+        transform.position = new Vector3(Random.Range(-range, range), Random.Range(-range, range), Random.Range(-range, range));
+    }
+
+    private void Update() {
+        transform.Translate(new Vector3(speed, 0f, 0f));
+
+        if (transform.position.x > range || transform.position.x < -range) speed *= -1f;
+    }
+
+}
