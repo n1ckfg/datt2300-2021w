c53cab7eacd83b4986876e59815d1d3be33314e3
Nick Fox-Gieg <nick@fox-gieg.com>
2021-04-05 15:14:33 -0400

    test

--- week11/example11-002/Assets/Scripts/Parallaxer.cs
+++ week11/example11-002/Assets/Scripts/Parallaxer.cs
@@ -6,34 +6,38 @@ public class Parallaxer : MonoBehaviour {
 
     public float speed;
 
-    private SpriteRenderer ren;
+    private Parallaxer dupeLayer;
+    private bool isDupe = false;
     private float spriteWidth;
-    private float xStart, xEnd;
+    private float startX, endX, offsetX;
     private Vector2 delta = Vector2.zero;
 
     private void Start() {
-        ren = GetComponent<SpriteRenderer>();
-        spriteWidth = ren.sprite.rect.width / 200f; // half pixel width / 100
-        updateDelta();
-        Debug.Log("spriteWidth: " + -spriteWidth + ", " + spriteWidth + " | start/end: " + xStart + ", " + xEnd);
-    }
+        if (gameObject.name.Split('(')[0] != transform.parent.name.Split('(')[0]) {
+            dupeLayer = GameObject.Instantiate(gameObject).GetComponent<Parallaxer>();
+            dupeLayer.isDupe = true;
+            dupeLayer.transform.SetParent(transform);
 
-    private void Update() {
-        updateDelta();
+            SpriteRenderer ren = GetComponent<SpriteRenderer>();
+            spriteWidth = ren.sprite.rect.width / 200f; // half pixel width / 100
 
-        transform.Translate(delta);
-
-        if (transform.position.x < xEnd) {
-            Vector2 pos = new Vector2(xStart, transform.position.y);
-            transform.position = pos;
+            startX = transform.position.x;
+            dupeLayer.startX = startX + spriteWidth * 2f;
+            dupeLayer.transform.position = new Vector2(dupeLayer.startX, transform.position.y);
         }
     }
 
-    private void updateDelta() {
+    private void Update() {
         delta = Vector2.left * speed * Time.deltaTime;
-        //Vector3 screenPos = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
-        xStart = spriteWidth - (Mathf.Abs(spriteWidth - Screen.width / 100f));
-        xEnd = -xStart;
+        offsetX = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z)).x / 2f;
+        endX = (startX - (spriteWidth * 2)) - offsetX;
+        Debug.Log("spriteWidth: " + spriteWidth + ", startX: " + startX + ", endX: " + endX + ", offsetX: " + offsetX);
+
+        transform.Translate(delta);
+
+        if (transform.position.x < endX) {
+            transform.position = new Vector2(startX, transform.position.y);
+        }
     }
 
 }
