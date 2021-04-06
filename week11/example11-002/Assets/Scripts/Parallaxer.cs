289dcc5f2e8a11a5cad5bb86a0f03d77401db9d0
Nick Fox-Gieg <nick@fox-gieg.com>
2021-04-05 23:00:57 -0400

    scrolling seems to work

--- week11/example11-002/Assets/Scripts/Parallaxer.cs
+++ week11/example11-002/Assets/Scripts/Parallaxer.cs
@@ -11,6 +11,7 @@ public class Parallaxer : MonoBehaviour {
     private float spriteWidth;
     private float startX, endX, offsetX;
     private Vector2 delta = Vector2.zero;
+    private bool flip = false;
 
     private void Start() {
         if (gameObject.name.Split('(')[0] != transform.parent.name.Split('(')[0]) {
@@ -19,24 +20,34 @@ public class Parallaxer : MonoBehaviour {
             dupeLayer.transform.SetParent(transform);
 
             SpriteRenderer ren = GetComponent<SpriteRenderer>();
-            spriteWidth = ren.sprite.rect.width / 200f; // half pixel width / 100
+            spriteWidth = ren.sprite.rect.width / 100f; // half pixel width / 100
 
+            transform.Translate(new Vector3(spriteWidth, 0f, 0f));
             startX = transform.position.x;
-            dupeLayer.startX = startX + spriteWidth * 2f;
+            dupeLayer.startX = startX - spriteWidth;
             dupeLayer.transform.position = new Vector2(dupeLayer.startX, transform.position.y);
         }
     }
 
     private void Update() {
-        delta = Vector2.left * speed * Time.deltaTime;
-        offsetX = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z)).x / 2f;
-        endX = (startX - (spriteWidth * 2)) - offsetX;
-        Debug.Log("spriteWidth: " + spriteWidth + ", startX: " + startX + ", endX: " + endX + ", offsetX: " + offsetX);
+        if (!isDupe) {
+            delta = Vector2.left * speed * Time.deltaTime;
+            offsetX = (Screen.width / Camera.main.orthographicSize) / 100f;
+            endX = startX - spriteWidth;
+            Debug.Log("spriteWidth: " + spriteWidth + ", startX: " + startX + ", endX: " + endX + ", offsetX: " + offsetX);
 
-        transform.Translate(delta);
+            transform.Translate(delta);
 
-        if (transform.position.x < endX) {
-            transform.position = new Vector2(startX, transform.position.y);
+            if (transform.position.x < endX) {
+                if (flip) {
+                    flip = false;
+				} else {
+                    dupeLayer.startX += spriteWidth * 2;
+                    flip = true;
+				}
+
+				transform.position = new Vector2(startX, transform.position.y);
+            }
         }
     }
 
